32fa897829e9eea912112fc228c6f22729c109c6
Alan Nunez <axn3@cbord.com>
2022-02-19 20:05:30 -0400

    Upgraded UI

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by QISM1 on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
