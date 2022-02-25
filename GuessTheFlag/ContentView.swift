82c87835bd583c3799d897bcf1f190152c5ad5f4
Alan Nunez <axn3@cbord.com>
2022-02-24 23:10:02 -0400

    Custom container

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+struct GridStack<Content: View>:View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+
+            }
+        }
+    }
+}
 struct ContentView: View {
     @State private var showingScore = false
     @State private var showTitle = ""
